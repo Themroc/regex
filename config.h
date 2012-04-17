affe5ac263f6d6a4eff16fd099704d80c8d7039f
Your Name <7hemroc@gmail.com>
2012-04-17 13:45:35 +0200

    New! Improved! ... now actually working

--- config.h
+++ config.h
@@ -1,2 +1,4 @@
-#define PACKAGE_NAME "regex"
-#define PACKAGE_VERSION "2.6.0"
+#define PACKAGE_NAME regex
+#define PACKAGE_VERSION 2.6.0
+#define REX_LIBNAME "regex"
+#define REX_OPENLIB luaopen_regex
